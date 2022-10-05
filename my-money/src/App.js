89c6e3bbafb7318661e19cbcb7d8e526fa7582d2
Ernesto Sustaita <ernesto.sustaita@yahoo.fr>
2022-10-05 00:05:25 -0500

    React install

---
+++ my-money/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>MyMoney</p>
+        <span>Start</span>
+      </header>
+    </div>
+  );
+};
